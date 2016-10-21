645428cf6a82e1fd99fc26390b4b9fac340fdd45
Corey Moten <clmoten@gmail.com>
2016-10-20 20:36:48 -0400

    Modified pom file to include AWS libraries. Added packages to src directory.

--- lambda_apis/ChatterForumService/src/com/chatter/forumservice/handler/ChatterForumServiceRequestHandler.java
+++ lambda_apis/ChatterForumService/src/com/chatter/forumservice/handler/ChatterForumServiceRequestHandler.java
@@ -1,4 +1,4 @@
-package com.chatter.forumservice;
+package com.chatter.forumservice.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
