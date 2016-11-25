ff5a487f96f0bac7b5535652b44b5980bb22b2db
Corey Moten <clmoten@gmail.com>
2016-11-24 23:01:46 -0500

    Modified ChatterForumTable script to include global secondary index creation.

--- db_scripts/table_scripts/ChatterForumTable.js
+++ db_scripts/table_scripts/ChatterForumTable.js
@@ -19,3 +19,62 @@ dynamodb.createTable(params, function(err, data) {
     else
         console.log(JSON.stringify(data, null, 2));
 });
+
+//Create created_by_index global secondary index
+var params = {
+    TableName: "Chatter_Forum",
+    AttributeDefinitions:[
+        {AttributeName: "created_by", AttributeType: "S"},
+        {AttributeName: "time_stamp", AttributeType: "N"},
+        {AttributeName: "title", AttributeType: "S"}
+    ],
+    GlobalSecondaryIndexUpdates: [
+        {
+            Create: {
+                IndexName: "created_by_index",
+                KeySchema: [
+                    {
+                        AttributeName: "created_by", 
+                        KeyType: "HASH" 
+                    }, 
+                    {
+                        AttributeName: "time_stamp",
+                        KeyType: "RANGE"
+                    }
+                ],
+                Projection: {
+                    "ProjectionType": "ALL"
+                },
+                ProvisionedThroughput: {
+                    "ReadCapacityUnits": 25,
+                    "WriteCapacityUnits": 25
+                }
+            }
+        },
+        {
+            Create: {
+                IndexName: "title_index",
+                KeySchema: [
+                    {
+                        AttributeName: "title",
+                        KeyType: "HASH"
+                    }
+                ],
+                Projection: {
+                    "ProjectionType": "ALL"
+                },
+                ProvisionedThroughput: {
+                    "ReadCapacityUnits": 25,
+                    "WriteCapacityUnits": 25
+                }
+            }
+        }
+    ]
+};
+
+dynamodb.updateTable(params, function(err, data) {
+    if (err)
+        console.log(JSON.stringify(err, null, 2));
+    else
+        console.log(JSON.stringify(data, null, 2));
+});
